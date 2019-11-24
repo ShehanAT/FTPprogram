f86ead931e0e9fd0cc7ae738af63076814e196f8
Shehan Atuk <shehanatuk@gmail.com>
2019-11-24 12:00:13 -0500

    added to simpleProgram.py

--- simpleProgram.py
+++ simpleProgram.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python 
 """
 A simple window in PyQt5
 Author: Jan Bodnar 
@@ -6,14 +7,17 @@ Author: Jan Bodnar
 
 import sys 
 from PyQt5.QtWidgets import QApplication, QWidget 
-
+from PyQt5.QtCore import *
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     w = QWidget()
     w.resize(250, 150)
     w.move(300, 300)
-    w.setWindowTitle('Simple')
+    w.setWindowTitle('Whatever I want')
     w.show()
+
+    x = str()
+    y = QDate()
 
     sys.exit(app.exec_())
