e3d38b4d44cc7f8239dc0a55f23101429b9aba44
Shehan Atuk <shehanatuk@gmail.com>
2019-11-24 12:29:54 -0500

    added to simpleProgram.py

--- simpleProgram.py
+++ simpleProgram.py
@@ -9,15 +9,23 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget 
 from PyQt5.QtCore import *
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    #app = QApplication(sys.argv)
 
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Whatever I want')
-    w.show()
+   # w = QWidget()
+   # w.resize(250, 150)
+   # w.move(300, 300)
+   # w.setWindowTitle('Whatever I want')
+   # w.show()
 
-    x = str()
+    x = str("apple")
+    b = "Fu\u00dfb\u00e4lle"
+    fruit = ["Apple", "Hawthorn", "Loquat", "Medlar", "Pear", "Quince"]
+    fruit2 = ["Apple", "Hawthorn", "Loquat", "Medlar", "Pear", "Quince"]
+    carriage = fruit 
+    carriage[2] = 'Shehan' # shallow copying 
+    print(fruit, carriage)
+    carriage = fruit2[:]
+    carriage[2] = 'Shehan' # deep copying
+    print(fruit2, carriage)
     y = QDate()
-
-    sys.exit(app.exec_())
+    #sys.exit(app.exec_())
