9990d7a584551679c85dff01b447a785352130b1
Shehan Atuk <shehanatuk@gmail.com>
2020-04-23 11:20:22 -0400

    reset hard then deleted useless files

--- makeSimpleDesktopGUI.py
+++
@@ -1,4 +0,0 @@
-from PyQt5.QtWidgets import QApplication, QLabel
-app = QApplication([])
-label = QLabel('My first desktop Application')
-label.show()
--- test.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout 
-
-app = QApplication([])
-button = QPushButton('Click')
-
-def on_button_clicked():
-    alert = QMessageBox()
-    alert.setText('You clicked the button!')
-    alert.exec_()
